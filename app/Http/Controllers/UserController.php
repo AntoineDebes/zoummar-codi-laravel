5f5d989bb3d64361f0ff59d7d87f43bc55ee0a63
Antoinedebes <antoinedebes@gmail.com>
2021-11-14 19:17:23 +0200

    added the contents in the backend

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,16 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-
+use JWTAuth;
 class UserController extends Controller
 {
     public function register(Request $request)
     {
+        // file_put_contents(__DIR__.'/test.json', json_encode($request->data));
         $user = User::create([
-            'name' => $request->name,
-            'email'    => $request->email,
-            'password' => $request->password,
-            'phone'=> $request->phone,
+            'name' => $request->data["Name"],
+            'email'    => $request->data["Email"],
+            'password' => $request->data["Password"],
+            'phone'=> $request->data["Phone"],
         ]);
 
         $token = auth('user')->login($user);
@@ -21,15 +22,23 @@ class UserController extends Controller
         return $this->respondWithToken($token);
     }
 
-    public function login()
+    public function login(Request $request)
     {
-        $credentials = request(['email', 'password']);
+
+        file_put_contents(__DIR__.'/test.json', json_encode($request->all()));
+
+        $credentials = [
+            'email' => $request->data["Email"],
+            'password' => $request->data["Password"]
+        ];
+
+        // file_put_contents(__DIR__.'/test.json', json_encode($credentials));
 
         if (! $token = auth('user')->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
-        return $this->respondWithToken($token);
+        return $this->respondWithTokenAndInfo($token);
     }
 
     public function logout()
@@ -42,6 +51,18 @@ class UserController extends Controller
     protected function respondWithToken($token)
     {
         return response()->json([
+            'access_token' => $token,
+            'token_type'   => 'bearer',
+            'expires_in'   => auth('user')->factory()->getTTL() * 60
+        ]);
+    }
+
+
+    protected function respondWithTokenAndInfo($token)
+    {
+
+        return response()->json([
+            'user'         => auth('user')->user(),
             'access_token' => $token,
             'token_type'   => 'bearer',
             'expires_in'   => auth('user')->factory()->getTTL() * 60
